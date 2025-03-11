a84010813ca0d32fe30c00554409b48767796a54
Luana Gerber <luanagerber.dev@gmail.com>
2025-03-11 16:11:29 -0400

    Initial Commit

---
+++ bingar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bingar
+//
+//  Created by Luana Gerber on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bingar/bingarApp.swift
@@ -0,0 +1,17 @@
+//
+//  bingarApp.swift
+//  bingar
+//
+//  Created by Luana Gerber on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct bingarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
