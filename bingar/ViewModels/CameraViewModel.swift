6f0601c4518399eb87b4bbc501b777ec9a6ada15
Luana Gerber <luanagerber.dev@gmail.com>
2025-03-25 19:29:57 -0400

    new implementation on progress

--- bingar/ViewModels/CameraViewModel.swift
+++ bingar/ViewModels/CameraViewModel.swift
@@ -1,7 +1,123 @@
 //
 //  CameraViewModel.swift
-//  bingar
+//  Bingar
 //
-//  Created by Luana Gerber on 25/03/25.
+//  Created by Jaide Zardin on 19/03/25.
 //
 
+import Foundation
+import CoreImage
+import Observation
+import UIKit
+import AVFoundation
+import SwiftUI
+
+
+@Observable
+class CameraViewModel {
+    
+    var currentFrame: CIImage?
+    var detectedObject: [DetectedObject]?
+    let cameraManager = CameraManager()
+    private var objectDetector = DetectorService()
+    var thumbnailImage: Image?
+    var thumbnailUIImage: UIImage? = nil
+    var thumbnailCGImage: CGImage? = nil {
+        didSet {
+            if let cgImage = thumbnailCGImage {
+                print("cccccc")
+                thumbnailUIImage = UIImage(cgImage: cgImage)
+            }
+        }
+    }
+    
+    init() {
+        Task {
+            await handleCameraPreviews()
+        }
+        
+        Task {
+            await handleCameraPhotos()
+        }
+    }
+    
+    func handleCameraPreviews() async {
+        
+        await cameraManager.start()
+        
+        for await image in cameraManager.previewStream {
+            Task { @MainActor in
+                currentFrame = image
+                
+                processFrame()
+            }
+        }
+    }
+    
+    func handleCameraPhotos() async {
+        let unpackedPhotoStream = cameraManager.photoStream
+            .compactMap { self.unpackPhoto($0) }
+        
+        for await photoData in unpackedPhotoStream {
+            Task { @MainActor in
+                thumbnailImage = photoData.thumbnailImage
+            }
+        }
+    }
+    
+    func processFrame() {
+        if let image = currentFrame {
+            detectedObject = objectDetector.predict(image:  image)
+        }
+    }
+    
+    private func unpackPhoto(_ photo: AVCapturePhoto) -> PhotoData? {
+        guard let imageData = photo.fileDataRepresentation() else { return nil }
+
+        guard let previewCGImage = photo.previewCGImageRepresentation(),
+           let metadataOrientation = photo.metadata[String(kCGImagePropertyOrientation)] as? UInt32,
+              let cgImageOrientation = CGImagePropertyOrientation(rawValue: metadataOrientation) else { return nil }
+        let imageOrientation = Image.Orientation(cgImageOrientation)
+        let thumbnailImage = Image(decorative: previewCGImage, scale: 1, orientation: imageOrientation)
+        
+        thumbnailCGImage = previewCGImage
+        
+        let photoDimensions = photo.resolvedSettings.photoDimensions
+        let imageSize = (width: Int(photoDimensions.width), height: Int(photoDimensions.height))
+        let previewDimensions = photo.resolvedSettings.previewDimensions
+        let thumbnailSize = (width: Int(previewDimensions.width), height: Int(previewDimensions.height))
+        
+        return PhotoData(thumbnailImage: thumbnailImage, thumbnailSize: thumbnailSize, imageData: imageData, imageSize: imageSize)
+    }
+}
+
+fileprivate struct PhotoData {
+    var thumbnailImage: Image
+    var thumbnailSize: (width: Int, height: Int)
+    var imageData: Data
+    var imageSize: (width: Int, height: Int)
+}
+
+fileprivate extension CIImage {
+    var image: Image? {
+        let ciContext = CIContext()
+        guard let cgImage = ciContext.createCGImage(self, from: self.extent) else { return nil }
+        return Image(decorative: cgImage, scale: 1, orientation: .up)
+    }
+}
+
+fileprivate extension Image.Orientation {
+
+    init(_ cgImageOrientation: CGImagePropertyOrientation) {
+        switch cgImageOrientation {
+        case .up: self = .right
+        case .upMirrored: self = .rightMirrored
+        case .down: self = .left
+        case .downMirrored: self = .leftMirrored
+        case .left: self = .up
+        case .leftMirrored: self = .upMirrored
+        case .right: self = .down
+        case .rightMirrored: self = .downMirrored
+        }
+    }
+}
