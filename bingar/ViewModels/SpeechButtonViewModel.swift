b4534e4aa90c52f478b6770064756bd936821955
Luana Gerber <luanagerber.dev@gmail.com>
2025-03-25 20:22:39 -0400

    error fixed on SpeechButtonViewModel

--- bingar/ViewModels/SpeechButtonViewModel.swift
+++ bingar/ViewModels/SpeechButtonViewModel.swift
@@ -24,7 +24,6 @@ class SpeechButtonViewModel: ObservableObject {
             
             let extractedNumber = processText(text: transcript)
             bingoViewModel.addSortedNumber(number: extractedNumber)
-
         } else {
             speechTranscriptor.resetTranscript()
             speechTranscriptor.startTranscribing()
